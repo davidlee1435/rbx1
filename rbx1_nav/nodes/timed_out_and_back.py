8c57bab804f475bac156d121f899917e24eb85b0
David Lee <jl4397@columbia.edu>
2018-09-19 16:57:48 -0400

    Rachel's changes

--- rbx1_nav/nodes/timed_out_and_back.py
+++ rbx1_nav/nodes/timed_out_and_back.py
@@ -2,10 +2,10 @@
 
 """ timed_out_and_back.py - Version 1.2 2014-12-14
 
-    A basic demo of the using odometry data to move the robot along
+    A modified basic demo of the using odometry data to move the robot along
     and out-and-back trajectory.
 
-    Created for the Pi Robot Project: http://www.pirobot.org
+    Original code created for the Pi Robot Project: http://www.pirobot.org
     Copyright (c) 2012 Patrick Goebel.  All rights reserved.
 
     This program is free software; you can redistribute it and/or modify
@@ -25,6 +25,8 @@
 import rospy
 from geometry_msgs.msg import Twist
 from math import pi
+import os
+import math 
 
 class OutAndBack():
     def __init__(self):
@@ -38,78 +40,81 @@ class OutAndBack():
         self.cmd_vel = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
         
         # How fast will we update the robot's movement?
-        rate = 50
+        self.rate = 50
         
         # Set the equivalent ROS rate variable
-        r = rospy.Rate(rate)
+        self.r = rospy.Rate(self.rate)
         
         # Set the forward linear speed to 0.2 meters per second 
-        linear_speed = 0.2
-        
-        # Set the travel distance to 1.0 meters
-        goal_distance = 1.0
-        
-        # How long should it take us to get there?
-        linear_duration = goal_distance / linear_speed
-        
-        # Set the rotation speed to 1.0 radians per second
-        angular_speed = 1.0
-        
-        # Set the rotation angle to Pi radians (180 degrees)
-        goal_angle = pi
-        
-        # How long should it take to rotate?
-        angular_duration = goal_angle / angular_speed
-     
-        # Loop through the two legs of the trip  
-        for i in range(2):
-            # Initialize the movement command
-            move_cmd = Twist()
-            
-            # Set the forward speed
-            move_cmd.linear.x = linear_speed
-            
-            # Move forward for a time to go the desired distance
-            ticks = int(linear_duration * rate)
-            
-            for t in range(ticks):
-                self.cmd_vel.publish(move_cmd)
-                r.sleep()
-            
-            # Stop the robot before the rotation
-            move_cmd = Twist()
-            self.cmd_vel.publish(move_cmd)
-            rospy.sleep(1)
-            
-            # Now rotate left roughly 180 degrees  
-            
-            # Set the angular speed
-            move_cmd.angular.z = angular_speed
+        self.linear_speed = 0.2
+	
+	# Set the rotation speed to 1.0 radians per second
+	self.angular_speed = 1.0
 
-            # Rotate for a time to go 180 degrees
-            ticks = int(goal_angle * rate)
-            
-            for t in range(ticks):           
-                self.cmd_vel.publish(move_cmd)
-                r.sleep()
-                
-            # Stop the robot before the next leg
-            move_cmd = Twist()
-            self.cmd_vel.publish(move_cmd)
-            rospy.sleep(1)    
-            
-        # Stop the robot
-        self.cmd_vel.publish(Twist())
+	while not rospy.is_shutdown():
+                move = raw_input('Enter T for translation, R for rotation, or Q for quit: ')
+		if move == 'T' or move == 't':
+                        distance = float(input('Distance? '))
+                        print(distance)
+			self.translate(distance)
+                elif move == 'R' or move == 'r':
+                        angle = float(input('Angle? '))
+                        self.rotate(angle)
+                elif move == 'Q' or move == 'q':
+                        break
+                else:
+                        print('Move not recognized. Try T, R, or Q.')
+	
+
+    def rotate(self, goal_angle):
+	angular_duration = goal_angle / self.angular_speed
+	angular_duration = math.fabs(angular_duration)
+
+	move_cmd = Twist()
+	if goal_angle < 0:
+		move_cmd.angular.z = self.angular_speed * -1.0
+	else:
+		move_cmd.angular.z = self.angular_speed 
+	ticks = int(angular_duration * self.rate)
+	
+	for t in range(ticks):
+		self.cmd_vel.publish(move_cmd)
+		self.r.sleep()
+	self.cmd_vel.publish(Twist())
+	
+    def translate(self, goal_distance):
+        linear_duration = goal_distance / self.linear_speed
+        linear_duration = math.fabs(linear_duration)
+
+        move_cmd = Twist()
+	if goal_distance < 0:
+		move_cmd.linear.x = self.linear_speed * -1.0
+	else:
+		move_cmd.linear.x = self.linear_speed
         
+	ticks = int(linear_duration * self.rate)
+
+        for t in range(ticks):
+            self.cmd_vel.publish(move_cmd)
+            self.r.sleep()
+        self.cmd_vel.publish(Twist())
+            
     def shutdown(self):
         # Always stop the robot when shutting down the node.
         rospy.loginfo("Stopping the robot...")
         self.cmd_vel.publish(Twist())
         rospy.sleep(1)
+
  
 if __name__ == '__main__':
+    clear = os.system("killall -9 rviz")
+    clear = os.system("killall -9 roscore")
+    clear2 = os.system("killall -9 rosmaster")
+    launch = os.system("roslaunch rbx1_bringup fake_turtlebot.launch &")
+    result = os.system("rosrun rviz rviz -d `rospack find rbx1_nav`/sim.rviz &")
+    
     try:
-        OutAndBack()
+	OutAndBack()
     except:
         rospy.loginfo("Out-and-Back node terminated.")
 
