f63b31cac62e75dc65800ba213f9dc38102f9ba3
David Lee <juhyunglee1435@gmail.com>
2018-10-01 16:05:35 -0400

    Adding hw2

--- hw2/hw2.py
+++ hw2/hw2.py
@@ -5,44 +5,80 @@ from sensor_msgs import LaserScan
 from math import pi
 
 class Bug2():
+import rospy
+from geometry_msgs.msg import Twist
+from math import pi
+import os
+import math 
+
+class OutAndBack():
     def __init__(self):
         # Give the node a name
-        rospy.init_node('bug2', anonymous=False)
+        rospy.init_node('out_and_back', anonymous=False)
 
-        # Set rospy to exectute a shutdown function when exiting       
+        # Set rospy to execute a shutdown function when exiting       
         rospy.on_shutdown(self.shutdown)
         
         # Publisher to control the robot's speed
-        self.cmd_vel = rospy.Publisher('/cmd_vel', Twist)
+        self.cmd_vel = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
         
         # How fast will we update the robot's movement?
-        rate = 50
+        self.rate = 50
         
         # Set the equivalent ROS rate variable
-        r = rospy.Rate(rate)
+        self.r = rospy.Rate(self.rate)
         
         # Set the forward linear speed to 0.2 meters per second 
-        linear_speed = 0.2
+        self.linear_speed = 0.2
+	
+	# Set the rotation speed to 1.0 radians per second
+	self.angular_speed = 1.0
+
+	self.run_pathfinder()
+
+    def run_pathfinder(self):
+	pass
+
+    def rotate(self, angle_in_radians):
+	goal_angle = angle_in_radians * float(pi/180)
+	angular_duration = goal_angle / self.angular_speed
+	angular_duration = math.fabs(angular_duration)
+
+	move_cmd = Twist()
+	if goal_angle < 0:
+		move_cmd.angular.z = self.angular_speed * -1.0
+	else:
+		move_cmd.angular.z = self.angular_speed 
+	ticks = int(angular_duration * self.rate)
+	
+	for t in range(ticks):
+		self.cmd_vel.publish(move_cmd)
+		self.r.sleep()
+	self.cmd_vel.publish(Twist())
+	
+    def translate(self, goal_distance):
+        linear_duration = goal_distance / self.linear_speed
+        linear_duration = math.fabs(linear_duration)
+
+        move_cmd = Twist()
+	if goal_distance < 0:
+		move_cmd.linear.x = self.linear_speed * -1.0
+	else:
+		move_cmd.linear.x = self.linear_speed
         
-        # Set the travel distance to 1.0 meters
-        goal_distance = 1.0
-        
-        # How long should it take us to get there?
-        linear_duration = goal_distance / linear_speed
-        
-        # Set the rotation speed to 1.0 radians per second
-        angular_speed = 1.0
-        
-        # Set the rotation angle to Pi radians (180 degrees)
-        goal_angle = pi
-        
-        # How long should it take to rotate?
-        angular_duration = goal_angle / angular_speed
-     
-        self.run_pathfinder() 
-        # Stop the robot
+	ticks = int(linear_duration * self.rate)
+
+        for t in range(ticks):
+            self.cmd_vel.publish(move_cmd)
+            self.r.sleep()
         self.cmd_vel.publish(Twist())
-    
+            
+    def shutdown(self):
+        # Always stop the robot when shutting down the node.
+        rospy.loginfo("Stopping the robot...")
+        self.cmd_vel.publish(Twist())
+        rospy.sleep(1)
+
     def run_pathfinder(self):
 	"""
 	Pseudocode
@@ -54,6 +90,7 @@ class Bug2():
 		self.move_on_m_line()
 		
 	"""
+	pass
 
 
     def shutdown(self):
@@ -66,5 +103,5 @@ if __name__ == '__main__':
     try:
         Bug2()
     except:
-        rospy.loginfo("Out-and-Back node terminated.")
+        rospy.loginfo("Bug2 node terminated.")
 
